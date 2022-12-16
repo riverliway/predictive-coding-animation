f6e1eed46ed4b395d0ae64df6b39b5703871d22b
River <riverliway@gmail.com>
2022-12-16 09:37:39 -0800

    update neural activities

--- prototype/scene.py
+++ prototype/scene.py
@@ -87,7 +87,7 @@ class pnet(Scene):
     self.play(AnimationGroup(*[update_error_anim(p, u, e) for (p, u, e) in zip(flat(predicted_nodes), flat(updated_nodes), flat(errors))]))
 
     # Move dots backwards from one triangle to another
-    self.inference_backprop(updated_nodes, errors, weights)
+    self.inference_back(updated_nodes, errors, weights)
 
     # Mutate triangle error colors after backwards pass
     animations = []
@@ -96,6 +96,23 @@ class pnet(Scene):
       max_error = max(map(lambda x: gray_value(x.get_fill_color()), errors[i + 1]))
       for error in errors[i]:
         animations.append(error.animate.set_fill_color(interpolate_color(BLACK, RED, random.uniform(0, max_error))))
+
+    self.play(AnimationGroup(*animations))
+
+    # Move the dots from the errors to the nodes for updating
+    self.inference_update(updated_nodes, errors, weights)
+
+    random.seed(66)
+
+    # Mutate inner neural activities after error update
+    new_updated_nodes = [[n.copy() for n in layer] for layer in updated_nodes]
+    animations = []
+    for i in range(1, len(new_updated_nodes) - 1):
+      for (node, error_node) in zip(new_updated_nodes[i], errors[i]):
+        self.add(node)
+        # Randomly mutate the updated node color but limit it to error of the node it is attached to
+        error = gray_value(error_node.get_fill_color())
+        animations.append(node.animate.set_fill_color(rand_gray(node.get_fill_color(), error)))
 
     self.play(AnimationGroup(*animations))
     
@@ -110,7 +127,7 @@ class pnet(Scene):
     # Pulse traveling within each neuron
     self.play(AnimationGroup(*[p.animate.shift(e.get_center() - p.get_center()) for (p, e) in zip(flat(error_pulses), flat(errors[1:]))]))
 
-  def inference_backprop(self, updated_nodes: list[list[Circle]], errors: list[list[Triangle]], weights: list[list[Line]]):
+  def inference_back(self, updated_nodes: list[list[Circle]], errors: list[list[Triangle]], weights: list[list[Line]]):
     # Create the dots and trails representing the pulse to the error node
     error_pulses = [[Dot(n.get_center(), z_index=7, color=RED, fill_opacity=1) for n in layer] for layer in errors[2:]]
     create_trail = lambda p, w: TracedPath(p.get_center, dissipating_time=0.5, z_index=6, stroke_color=RED, stroke_width=w.get_stroke_width(), stroke_opacity=[0, 1])
@@ -134,6 +151,17 @@ class pnet(Scene):
 
     self.play(AnimationGroup(*animations))
 
+  def inference_update(self, updated_nodes: list[list[Circle]], errors: list[list[Triangle]], weights: list[list[Line]]):
+    # Create the dots and trails representing the pulse from the error to the node
+    error_pulses = [[Dot(n.get_center(), z_index=7, color=RED, fill_opacity=1) for n in layer] for layer in errors[1:-1]]
+    create_trail = lambda p, w: TracedPath(p.get_center, dissipating_time=0.5, z_index=6, stroke_color=RED, stroke_width=w.get_stroke_width(), stroke_opacity=[0, 1])
+    error_trails = [create_trail(p, w) for i in range(len(error_pulses)) for (p, w) in zip(error_pulses[i], weights[(i + 1) * 2])]
+    for (pulse, trail) in zip(flat(error_pulses), error_trails):
+      self.add(pulse, trail)
+
+    # Pulse traveling within each neuron
+    self.play(AnimationGroup(*[p.animate.shift(e.get_center() - p.get_center()) for (p, e) in zip(flat(error_pulses), flat(updated_nodes[1:-1]))]))
+
 T = TypeVar('T')
 def flat(arrays: list[list[T]]) -> list[T]:
   return [item for sublist in arrays for item in sublist]
@@ -150,9 +178,9 @@ def rand_gray(original: color.Color | None = None, range: float | None = None):
 
   direction = random.randint(0, 1)
   if direction == 0:
-    return interpolate_color(BLACK, original, random.uniform(0, 1) * range)
+    return interpolate_color(original, BLACK, random.uniform(0, 1) * range)
 
-  return interpolate_color(WHITE, original, random.uniform(0, 1) * range)
+  return interpolate_color(original, WHITE, random.uniform(0, 1) * range)
 
 def calc_error(predicted: Circle, actual: Circle) -> float:
   return abs(gray_value(predicted.get_fill_color()) - gray_value(actual.get_fill_color()))
