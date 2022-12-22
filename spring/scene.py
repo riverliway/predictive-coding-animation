ae796546de0bed04c872348088427336c4126b8b
River <riverliway@gmail.com>
2022-12-21 21:22:12 -0800

    animate weights for spring

--- spring/scene.py
+++ spring/scene.py
@@ -1,14 +1,20 @@
 from typing import TypeVar
 from manim import *
+import numpy as np
 
 class spring(Scene):
   def construct(self):
 
-    self.construct_baseplates()
+    neuron_locations = self.construct_baseplates()
+    self.create_neurons(neuron_locations)
     
     self.wait()
 
-  def construct_baseplates(self):
+  def construct_baseplates(self) -> list[np.ndarray]:
+    """
+    Constructs the base rectangles and the scales for each neuron
+    Returns: a list of coords for where the neurons go
+    """
     base = RoundedRectangle(corner_radius=0.1, color=DARK_GRAY, width=1.5, height=2).set_opacity(1).apply_matrix([[1, 1], [0, 2]])
     rect1 = base.copy().shift(1.3 * DOWN + 3 * LEFT)
     rect2 = base.copy().shift(1.3 * DOWN + 1 * LEFT)
@@ -26,6 +32,26 @@ class spring(Scene):
     scale4 = scale2.copy().shift(RIGHT * 3.25 + 1.5 * DOWN)
 
     self.add(scale1, scale2, scale3, scale4)
+
+    return [s.get_center() + UP * 1.5 for s in [scale1, scale2, scale3, scale4]]
+
+  def create_neurons(self, neuron_locations: list[np.ndarray]): 
+    neuron = Circle(radius=0.5, color=WHITE).set_opacity(1)
+    neuron0 = neuron.copy().shift(neuron_locations[0])
+    neuron1 = neuron.copy().shift(neuron_locations[1])
+    neuron2 = neuron.copy().shift(neuron_locations[2])
+    neuron3 = neuron.copy().shift(neuron_locations[3])
+    neurons = [neuron0, neuron1, neuron2, neuron3]
+
+    self.play(AnimationGroup(*[Create(n) for n in neurons], lag_ratio=0.1))
+
+    weight0 = Arrow(neuron0, neuron1, buff=0, color=YELLOW)
+    weight1 = Arrow(neuron1, neuron2, buff=0, color=YELLOW)
+    weight2 = Arrow(neuron1, neuron3, buff=0, color=YELLOW)
+
+    self.play(AnimationGroup(Create(weight0), Create(VGroup(weight1, weight2)), lag_ratio=0.1))
+
+    self.play(neuron1.animate.shift(DOWN))
 
 def neuron_color(activity: float) -> color.Color:
   return interpolate_color(BLUE_E, WHITE, activity)
